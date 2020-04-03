f08f16fb12944f473726a1a12970f99bb6c9a2e8
aurora10 <aurora10@gmail.com>
2020-04-03 18:46:01 +0200

    frond end working

---
+++ app/Http/Controllers/BlogController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Post;
+use Illuminate\Http\Request;
+
+class BlogController extends Controller
+{
+
+    protected $limit = 3;
+
+    public function index() {
+
+
+
+//        \DB::enableQueryLog();
+         $posts = Post::with('author')->latestFirst()->published()->simplePaginate($this->limit);
+
+         return view('blog.index', compact('posts'));
+        //dd(\DB::getQueryLog());
+    }
+
+    public function show(Post $post) {
+//model injection method
+        //$post = Post::published()->findOrFail($id);
+
+
+
+        return view('blog.show', compact('post'));
+
+    }
+
+
+}
---
+++ database/seeds/PostsTableSeeder.php
@@ -0,0 +1,48 @@
+<?php
+
+use Carbon\Carbon;
+use Faker\Factory;
+use Illuminate\Database\Seeder;
+
+class  PostsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('posts')->truncate();
+
+        $posts = [];
+        $faker = Factory::create();
+        $date = Carbon::create(2020, 3, 22, 9);
+        //generate 10 dummy posts
+        for ($i=1;$i<=10; $i++) {
+
+            $image = "Post_Image_" . rand(1, 5) . ".jpg";
+            $date->addDays(1);
+            $published_date = clone($date);
+            $createdDate = clone($date);
+
+            $posts[] = [
+                'author_id'=> rand(1,3),
+                'title'=> $faker->sentence(rand(8, 12)),
+                'excerpt'=> $faker->text(rand(250, 300)),
+                'body'=> $faker->paragraphs(rand(10, 15), true),
+                'slug'=> $faker->slug(),
+                'image'=> rand(0,1) == 1 ? $image : NULL,
+                'created_at' => $createdDate,
+                'updated_at' => $createdDate,
+                'published_at' => $i > 5 ?  $published_date : (rand(0,1) == 0 ? NULL : $published_date->addDays( 4)),
+
+
+
+            ];
+        }
+
+        DB::table('posts')->insert($posts);
+
+    }
+}
